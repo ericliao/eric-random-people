1717527920a71e405840724f96cab5e5cd359b8c
Eric Liao <the.eric.liao@gmail.com>
2015-05-03 16:30:56 +0100

    Don't need url in person view

--- src/views/card.js
+++ src/views/card.js
@@ -16,7 +16,7 @@ module.exports = React.createClass({
     render: function () {
         var view = this;
         var name = view.props.info.name;
-        var href = '#person/' + view.props.seed;
+        var href = view.props.seed ? '#person/' + view.props.seed : (void 0);
         return (
             <a className="person-panel" href={href}>
                 <h2>{name.title} {name.first} {name.last}</h2>
