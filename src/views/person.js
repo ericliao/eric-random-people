fcae3cce51f069ecff54c26c04a100847aa62a42
Eric Liao <the.eric.liao@gmail.com>
2015-05-03 12:05:29 +0100

    Added styling to people details

--- src/views/person.js
+++ src/views/person.js
@@ -3,15 +3,68 @@
 'use strict';
 var React = require('react');
 
+var Picture = React.createClass({
+    componentWillUnmount: function () {
+        $(this.getDOMNode()).remove();
+    },
+    render: function () {
+        var view = this;
+        //return (
+        //);
+    }
+});
+
 var Panel = React.createClass({
     componentWillUnmount: function () {
         $(this.getDOMNode()).remove();
     },
     render: function () {
         var view = this;
+        var name = view.props.details.name;
         return (
-            <li className="topcoat-list__item panel">
-                <h3>Email: {view.props.person.email}</h3>
+            <div className="person-panel">
+                <h1>{name.title} {name.first} {name.last}</h1>
+            </div>
+        );
+    }
+});
+
+var Person = React.createClass({
+    componentWillUnmount: function () {
+        $(this.getDOMNode()).remove();
+    },
+    render: function () {
+        var view = this;
+        var person = view.props.person;
+        var details = {
+            'name': person.name,
+            'location': person.location,
+            'picture': person.picture
+        };
+        // use moment to convert person.dob =
+
+        // list of additional person details
+        var more_details = {
+            'dob': ['Date of Birth', ''],
+            'phone': ['Phone', 'tel:'],
+            'email': ['Email', 'email:'],
+            'username': ['Username', ''],
+            'password': ['Password', '']
+        };
+        return (
+            <li className="topcoat-list__item person-view">
+                <Panel details={details} />
+                <ul className="topcoat-list__container">
+                    {_.map(more_details, function (detail, key) {
+                        var href = detail[1] !== '' ? detail[1] + person[key] : '#';
+                        return <li className="topcoat-list__item details-item" key={key}>
+                                <a href={href}>
+                                    <p>{detail[0]}:</p>
+                                    <p>{person[key]}</p>
+                                </a>
+                            </li>
+                    })}
+                </ul>
             </li>
         );
     }
@@ -26,7 +79,7 @@ module.exports = Backbone.View.extend({
         var view = this;
         var person = this.model.toJSON()[0];
         React.render(
-            <Panel person={person}/>,
+            <Person person={person}/>,
             this.el
         );
     }
